d15031f12b0c088510028c09299749d47ef1bf3b
Dixon G <dxxn0101@gmail.com>
2023-04-03 17:37:52 -0600

    Tarea - Crear un nuevo componente

--- 03-counter-app/src/App.jsx
+++ 03-counter-app/src/App.jsx
@@ -1,5 +1,12 @@
+import { FirstApp } from "./FirstApp";
+
 const App = () => {
-  return <h1>Hola Mundo con React</h1>;
+  return (
+    <>
+      <h1>Hola Mundo con React</h1>
+      <FirstApp />
+    </>
+  );
 };
 
 export default App;
