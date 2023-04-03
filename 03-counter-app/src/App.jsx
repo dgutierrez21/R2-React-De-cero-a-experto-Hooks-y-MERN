df7647daa4e979f84d1508443c2fd182b430d568
Dixon G <dxxn0101@gmail.com>
2023-04-03 17:43:23 -0600

    Retornar elementos en el Componente - Fragment

--- 03-counter-app/src/App.jsx
+++ 03-counter-app/src/App.jsx
@@ -1,10 +1,20 @@
 import { FirstApp } from "./FirstApp";
 
+// const App2 = () => {
+//   return (
+//       <h1>Hola Mundo con React</h1>
+//       <FirstApp />
+//   );
+// };
+
+// Un fragment en React es una característica que permite agrupar un conjunto de elementos hijos (child elements) de un componente sin crear un elemento padre (parent element) adicional en el árbol de elementos (DOM). Esto puede ser útil para mejorar el rendimiento y evitar renderizaciones innecesarias al evitar la creación de nodos HTML adicionales. En lugar de crear un nodo HTML adicional, el fragment simplemente envuelve los elementos hijos, sin introducir un nodo adicional en el árbol de elementos.
+
 const App = () => {
   return (
     <>
       <h1>Hola Mundo con React</h1>
       <FirstApp />
+      <p>Soy un subtitulo</p>
     </>
   );
 };
