8e60324c5c0f719459d39d0c44b352f4f3ca60f7
Dixon G <dxxn0101@gmail.com>
2023-04-03 17:33:49 -0600

    Nuestro primer Componente

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-  return <h1>Hola Mundo con React</h1>;
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
