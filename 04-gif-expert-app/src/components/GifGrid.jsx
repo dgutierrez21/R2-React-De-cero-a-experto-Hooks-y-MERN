801df5a0c7dad13c38b8ed6c36550a915139cacd
Dixon G <dxxn0101@gmail.com>
2023-04-05 08:40:01 -0600

    Mostrar los títulos de las imágenes

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -2,26 +2,26 @@ import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ categoria }) => {
-  const [counter, setCounter] = useState(10);
-  // useEffect es un hooks de react que sirve para disparar efectos secundarios
+  const [imagenes, setImagenes] = useState([]);
 
-  //--- Si el array de dependencias está vacío ([]), el efecto se ejecutará solo una vez, inmediatamente después de que el componente se haya montado en el DOM.
-
-  //--- Cuando se utiliza useEffect sin un array de dependencias, el efecto se ejecuta en cada renderizado del componente, incluyendo el primer renderizado y también cuando ocurren actualizaciones.
-
-  //--- Cuando el array de dependencias está presente, React solo volverá a ejecutar la función useEffect si alguna de las dependencias en el array cambia de valor.
+  const getImagenes = async () => {
+    const nuevasImagenes = await getGifs(categoria);
+    setImagenes(nuevasImagenes);
+  };
 
   useEffect(() => {
-    getGifs(categoria);
+    getImagenes();
   }, []);
 
   return (
     <>
       <h3>{categoria}</h3>
 
-      <h3>{counter}</h3>
-
-      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <ol>
+        {imagenes.map(({ id, titulo }) => (
+          <li key={id}>{titulo}</li>
+        ))}
+      </ol>
     </>
   );
 };
