98f36525185c745806dc123b4a33a0938d3ee122
Dixon G <dxxn0101@gmail.com>
2023-04-06 17:38:40 -0600

    Formulario con custom Hook

---
+++ 05-hooks-app/src/02-useEffect/FormWithCustomHooks.jsx
@@ -0,0 +1,46 @@
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHooks = () => {
+  const { formState, onInputChange } = useForm({
+    nombreUsuario: "",
+    email: "",
+    contrasenia: "",
+  });
+
+  const { nombreUsuario, email, contrasenia } = formState;
+
+  return (
+    <form>
+      <h1>Formmulario</h1>
+
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Nombre de usuario"
+        name="nombreUsuario"
+        value={nombreUsuario}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-4"
+        placeholder="correo@ejemplo.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="password"
+        className="form-control mt-4"
+        placeholder="Ingrese su contraseña"
+        name="contrasenia"
+        value={contrasenia}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
--- 05-hooks-app/src/HooksApp.jsx
+++ 05-hooks-app/src/HooksApp.jsx
@@ -1,11 +1,12 @@
 import { CounterApp } from "./01-useState/CounterApp";
 import { CounterWithCustomHooks } from "./01-useState/CounterWithCustomHooks";
+import { FormWithCustomHooks } from "./02-useEffect/FormWithCustomHooks";
 import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 export const HooksApp = () => {
   return (
     <>
-      <SimpleForm />
+      <FormWithCustomHooks />
     </>
   );
 };
