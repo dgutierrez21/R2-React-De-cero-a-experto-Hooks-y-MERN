4e7f0bb83ca809cc60045f767d986c28f81a0c7a
Dixon G <dxxn0101@gmail.com>
2023-04-07 13:10:15 -0600

    useFetch + useCounter

--- 05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
+  const { counter, incrementar, reiniciar } = useCounter(1);
+
   const { data, isLoading, error } = useFetch(
-    "https://api.breakingbadquotes.xyz/v1/quotes"
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
   );
 
   // se está utilizando la doble negación (!!), lo cual convierte el valor en un booleano. Si la variable data es nula, indefinida o vacía, la doble negación (!!data) la convertirá en false, mientras que si tiene algún valor, la doble negación la convertirá en true.
@@ -10,6 +14,12 @@ export const MultipleCustomHooks = () => {
   // esto asegura que data no sea nula, indefinida o vacía antes de acceder al primer elemento del arreglo y evita que la app se rompa debido a esto.
 
   const { author, quote } = !!data && data[0];
+
+  useEffect(() => {
+    reiniciar();
+  }, [counter]);
+
+  console.log(data);
 
   return (
     <>
@@ -31,6 +41,13 @@ export const MultipleCustomHooks = () => {
         <p className="mb-1">{quote}</p>
         <footer className="blockquote-footer mt-1">{author}</footer>
       </blockquote>
+
+      <button
+        className={`${isLoading ? "d-none" : "d-block"}`}
+        onClick={() => incrementar()}
+      >
+        Next Quote
+      </button>
     </>
   );
 };
