68a5982695db5ca076d9591842a92307eaa6e9af
Dixon G <dxxn0101@gmail.com>
2023-04-07 08:29:35 -0600

    mostrar quotes y añadir loading

--- 05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
@@ -5,12 +5,32 @@ export const MultipleCustomHooks = () => {
     "https://api.breakingbadquotes.xyz/v1/quotes"
   );
 
-  console.log({ data, isLoading, error });
+  // se está utilizando la doble negación (!!), lo cual convierte el valor en un booleano. Si la variable data es nula, indefinida o vacía, la doble negación (!!data) la convertirá en false, mientras que si tiene algún valor, la doble negación la convertirá en true.
+
+  // esto asegura que data no sea nula, indefinida o vacía antes de acceder al primer elemento del arreglo y evita que la app se rompa debido a esto.
+
+  const { author, quote } = !!data && data[0];
+
   return (
     <>
       <h1>BreakingBad Quotes</h1>
 
       <hr />
+
+      <div
+        className={`alert alert-info text-center ${
+          isLoading ? "d-block" : "d-none"
+        }`}
+      >
+        Loading...
+      </div>
+
+      <blockquote
+        className={`blockquote text-end ${isLoading ? "d-none" : "d-block"}`}
+      >
+        <p className="mb-1">{quote}</p>
+        <footer className="blockquote-footer mt-1">{author}</footer>
+      </blockquote>
     </>
   );
 };
