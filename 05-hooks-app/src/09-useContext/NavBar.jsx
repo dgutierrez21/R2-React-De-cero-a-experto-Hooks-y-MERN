0d80891e0df66946cc6bd2bf608d726ab783b662
Dixon G <dxxn0101@gmail.com>
2023-04-09 16:31:40 -0600

    NavLink

--- 05-hooks-app/src/09-useContext/NavBar.jsx
+++ 05-hooks-app/src/09-useContext/NavBar.jsx
@@ -1,12 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <div className="d-flex gap-3">
-      <Link to={"/"}>Home</Link>
-      <Link to={"about"}>About</Link>
-      <Link to={"login"}>Login</Link>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          useContext
+        </Link>
+
+        <div className="navbar-collapse">
+          <div className="navbar-nav">
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "activeLink" : ""}`
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "activeLink" : ""}`
+              }
+              to="about"
+            >
+              About
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "activeLink" : ""}`
+              }
+              to="login"
+            >
+              Login
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
