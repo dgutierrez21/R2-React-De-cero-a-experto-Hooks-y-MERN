a91c6a61cfaf87704ccc9b4073b0cfab2033b97d
Dixon G <dxxn0101@gmail.com>
2023-04-09 15:16:19 -0600

    Link

--- 05-hooks-app/src/09-useContext/MainApp.jsx
+++ 05-hooks-app/src/09-useContext/MainApp.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { NavBar } from "./NavBar";
 
 export const MainApp = () => {
   return (
     <>
       <h1>MaiApp</h1>
+      <NavBar />
       <hr />
 
       <Outlet />
