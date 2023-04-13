3c1299de6c72e8d5f170faae72caadf29ce0c0fd
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-04-13 17:31:13 -0600

    validar login

--- 06-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 06-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -10,6 +10,13 @@ export const LoginPage = () => {
   const handleLogin = () => {
     const nombre = prompt("Ingrese su nombre");
 
+    if (nombre === null) return;
+
+    if (nombre.trim().length < 3) {
+      alert("Ingrese un nombre válido.");
+      return;
+    }
+
     const lastPath = localStorage.getItem("lastPath") || "/";
 
     onLogin(nombre);
