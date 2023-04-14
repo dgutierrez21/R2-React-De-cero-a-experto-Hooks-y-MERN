16600ae43e11d41728d9b367b82af41122bdd6cd
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-04-14 12:06:35 -0600

    pokemonSlice

---
+++ 08-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,15 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+
+      <hr />
+
+      <ul>
+        <li>Pokemon 1</li>
+        <li>Pokemon 2</li>
+        <li>Pokemon 3</li>
+      </ul>
+    </>
+  );
+};
