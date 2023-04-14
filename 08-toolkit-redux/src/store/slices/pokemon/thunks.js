6a542903bb166f2cebfebda47fd492173dc69fd1
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-04-14 15:17:25 -0600

    Axios

--- 08-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 08-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,11 +1,25 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    // realizar petición http
+    // // realizar petición http con fetch
+    const url = `pokemon?limit=10&offset=${page * 10}`;
 
-    // dispatch(setPokemons())
+    // const resp = await fetch("https://pokeapi.co/api/v2/" + url);
+    // const data = await resp.json();
+
+    // console.log(data);
+
+    const { data } = await pokemonApi.get(url);
+
+    dispatch(
+      setPokemons({
+        pokemons: data.results,
+        page: page + 1,
+      })
+    );
   };
 };
